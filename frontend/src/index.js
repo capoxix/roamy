7d5d21f6c9b3b80b3ac864b063b919951c7a4b48
Garbo Cheng <garbocheng93@gmail.com>
2018-08-27 13:26:35 -0700

    add utils

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import {login} from './util/session_api_util';
+import {login, register, currentSession} from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     window.login = login;
+    window.register = register;
+    window.currentSession = currentSession;
     ReactDOM.render(<App />, root);
     
 
--- frontend/src/util/session_api_util.js
+++ frontend/src/util/session_api_util.js
@@ -1,7 +1,6 @@
 // const fetch = require('node-fetch');
 const axios = require('axios');
 
-
 //{email: 'garbocheng93@gmail.com', password: '123456'}
 export const login = user => {
     axios.post(`/api/users/login`, user)
@@ -9,6 +8,20 @@ export const login = user => {
         // console.log(response);
         console.log(response.data);
         // return response.data;
+    });
+};
+// {name: 'testing', email: 'testing1@gmail.com', password:'123456', password2: '123456'}
+export const register = user => {
+    axios.post(`/api/users/register`, user)
+    .then((response) => {
+        console.log(response.data);
+    });
+};
+
+export const currentSession = () => {
+    axios.get(`/api/users/current`)
+    .then((response) => {
+        console.log(response.data);
     });
 };
 
