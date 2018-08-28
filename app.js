5264f4de6a8a076efb3cd26362cf04066ff5f32d
Tony <tonywzhang@gmail.com>
2018-08-28 08:49:39 -0700

    morning commit

--- app.js
+++ app.js
@@ -20,31 +20,74 @@ mongoose
 
 const app = express();
 
-let minutes = 16;
+class Point {
+  constructor({lat, long}){
+    this.lat = lat;
+    this.long = long;
+    this.draw = false;
+  }
 
-//create route to get single book by its isbn
-app.get('/directions/', (request, response) => {
+  // this.distance = distance;
+  // this.time= time;
+}
+
+let point;
+
+
+let lat= 37.7990;
+let long= -122.4014;
+let minutes = 15;
+let searches = 0;
+let difference = minutes*0.005402;
+let scale = 0.005402;
+
+app.get(`/search`, (request, response) => {
   // make api call using fetch
-  fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=San+Francisco&destination=37.799809,%20-122.389125&key=AIzaSyDBghaO6vALAG_-QG2SCBN8LEB_jFM6o1Q`)
-  .then((response) => {
+  let googleMins = 0;
+
+
+  //
+  point = new Point({lat: lat,long: long+difference})
+
+  while(googleMins !== minutes && searches < 4) {
+    searches+=1;
+    point.long = long+difference;
+    console.log("Our first long is: ", point.long);
+    fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${lat}+${long}&destination=${point.lat},%20${point.long}&key=AIzaSyDBghaO6vALAG_-QG2SCBN8LEB_jFM6o1Q`)
+    .then((response) => {
       return response.text();
-  })
-  .then((body) => {
+    })
+    .then((body) => {
       let results = JSON.parse(body)
       // console.log(results)   // logs to server
       // console.log(results.routes[0].legs[0].end_address)
       console.log(results.routes[0].legs[0].duration.text.split(" ")[0]);
-      result_minutes = parseInt(results.routes[0].legs[0].duration.text.split(" ")[0]);
-      if (result_minutes === minutes){
-          response.send(results);
-        } else {
-          
-        };
+      console.log("Our lat is currently:", point.lat);
+      console.log("Our long is currently:", point.long);
+      console.log("Our diff is currently:", difference);
 
-      // sends to frontend
+      point.long = point.long - scale;
 
+      console.log("Our new long is: ", point.long);
+
+      googleMins = parseInt(results.routes[0].legs[0].duration.text.split(" ")[0]);
+      console.log("Our googlemins is currently:", googleMins);
+
+
+      // if (minutes === googleMins){
+      //   // endpoints.push(point);
+      //   response.send(point);
+      // } else {
+      //   difference = minutes*(point.long-long)/googleMins;
+      // }
     });
+  }
 });
+
+
+//create route to get single book by its isbn
+
+
 
 //Middleware for body parser
 app.use(bodyParser.urlencoded({extended: false}));
