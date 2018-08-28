b3e7a750432517a20ceb04ec02e5349d235abd51
Garbo Cheng <garbocheng93@gmail.com>
2018-08-27 22:43:56 -0700

    add location post route and validation

--- routes/api/locations.js
+++ routes/api/locations.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Location = require('../../models/Location');
+
+const validateTrackInput = require('../../validation/track.js');
 
 router.get('/test', (req, res) => res.json({msg: 'Locations route is working'}));
+
+router.post('/track', (req,res) => {
+
+    const{ errors, isValid } = validateTrackInput(req.body);
+
+    if(!isValid){
+        return res.status(400).json(errors);
+    }
+
+    const location = new Location({
+        name: req.body.name,
+        lat: req.body.lat,
+        lng: req.body.lng
+    });
+
+    location.save()
+        .then(location => res.json(location))
+        .catch(err => console.log(err));
+
+});
 
 module.exports = router;
