bba3f28474f8b6e550a0c10c83c0c00d3dda86d5
Kevin <kevin.ou705@gmail.com>
2018-08-27 18:06:16 -0700

    Create mapformcontainer and rename apposite files folders

---
+++ frontend/src/components/header/map-form-container.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+
+import MapForm from './map-form';
+
+const mapStateToProps = (state) => {
+  return ({});
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return ({});
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MapForm);
