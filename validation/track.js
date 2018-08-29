c90002cf01cf17cd232437502dfb1ca147b09128
Garbo Cheng <garbocheng93@gmail.com>
2018-08-29 09:45:40 -0700

    modify model and validation to have userId

--- validation/track.js
+++ validation/track.js
@@ -5,6 +5,7 @@ module.exports = function validateTrackInput(data) {
   let errors = {};
 
   data.name = !isEmpty(data.name) ? data.name: '';
+  data.userId = !isEmpty(data.userId) ? data.userId: '';
   data.lat = !isEmpty(data.lat) ? data.lat : '';
   data.lng = !isEmpty(data.lng) ? data.lng : '';
 
@@ -19,6 +20,10 @@ module.exports = function validateTrackInput(data) {
 
   if (Validator.isEmpty(data.lng)) {
     errors.lng= 'Longitude field is required';
+  }
+
+  if (Validator.isEmpty(data.userId)) {
+    errors.userId= 'User logged in is required';
   }
 
   return {
