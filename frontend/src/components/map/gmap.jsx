2eb798cd39ec6fad63a38a6709a1f210fe5c72fd
Garbo Cheng <garbocheng93@gmail.com>
2018-08-29 13:35:04 -0700

    delte  unused codes

--- frontend/src/components/map/gmap.jsx
+++ frontend/src/components/map/gmap.jsx
@@ -51,13 +51,13 @@ class GMap extends React.Component {
         let trackLocation =  {name:'tracking', lat: `${this.state.clicked.lat}`, lng: `${this.state.clicked.lng}`, userId: this.props.userId};
         track(trackLocation).then(res => console.log('tracked', res));
     }
-
+    /*get favorites locations of user & set to map */
     addFavoritesToMarkers(){
         let that = this;
         getFavorites(this.props.userId).then(favorites =>  that.setMarkersIntoMap(favorites.data));
     }
 
-
+    /*set markers into map */
     setMarkersIntoMap(favoriteDataArr){
         let favoritesMarkersArr = favoriteDataArr.map(favorite => 
             <Marker onClick={this.onMarkerClick}
@@ -90,105 +90,50 @@ class GMap extends React.Component {
     // let lat = 37.7749;
     // let lng = -122.4194;
     // let minutes = 15;
-    let points = [];
-  
-    let goldStar = {
-        path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
-        fillColor: 'yellow',
-        fillOpacity: 0.8,
-        scale: 1,
-        strokeColor: 'gold',
-        strokeWeight: 14
-      };
+    let points = []; //Point.initEndPoints(lat, lng, minutes);
 
-      const style = {
-        width: '800px',
-        height: '800px'
-      }
+    const style = {
+    width: '800px',
+    height: '800px'
+    }
 
-      const markers = [];
-      //[
-    //     <Marker onClick={this.onMarkerClick}
-    //     name={'AA'}
-    //     position={{lat: 37.7990, lng: -122.4014}} />,
-    //     <Marker onClick={this.onMarkerClick}
-    //     name={'YOUR LOCATION!'}
-    //     position={this.state.center}
-    //     icon= {{path: this.props.google.maps.SymbolPath.CIRCLE, scale:10}}/>
-    // ];
-
-
-    // let that = this;
-    // if(navigator.geolocation){
-    //     navigator.geolocation.getCurrentPosition(function(position) {
-    //         let pos = {
-    //             lat: position.coords.latitude,
-    //             lng: position.coords.longitude
-    //         }
-    //         that.setState({center: pos});
-    //     });
-
-        // console.log('userId', this.props.userId);
-        // console.log('trackFunction', this.props.track);
-
-        /* attempt to make a control in google maps*/
-        return (
+    /* attempt to make a control in google maps*/
+    return (
+        <div>
+            <button type='button' onClick={()=>this.trackInput()}>TRACK LOCATION</button>
+            <button type='button' onClick={()=> this.addFavoritesToMarkers()}>Get Favorite Spots</button>
+            <button type='button' onClick={()=> this.getCurrentLocation()}>Get Current Location</button>
             <div>
-                <button type='button' onClick={()=>this.trackInput()}>TRACK LOCATION</button>
-                <button type='button' onClick={()=> this.addFavoritesToMarkers()}>Get Favorite Spots</button>
-                <button type='button' onClick={()=> this.getCurrentLocation()}>Get Current Location</button>
-                <div>
-                    <Map google={this.props.google}
-                    onClick={this.onMapClicked}
-                    center={this.state.center}
-                    style={style}
-                    // controls[{this.props.google.maps.ControlPosition.TOP_CENTER}]
-                    >
-                        {markers}
-                        {this.state.currentLocationMarker}
-                        {this.state.favoriteMarkers}
-                        {this.state.clickedMarker}
+                <Map google={this.props.google}
+                onClick={this.onMapClicked}
+                center={this.state.center}
+                style={style}
+                // controls[{this.props.google.maps.ControlPosition.TOP_CENTER}]
+                >
+                    {this.state.currentLocationMarker}
+                    {this.state.favoriteMarkers}
+                    {this.state.clickedMarker}
 
-                        <InfoWindow
-                            marker={this.state.activeMarker}
-                            visible={this.state.showingInfoWindow}>
-                            <div>
-                                <h1>{this.state.selectedPlace.name}</h1>
-                            </div>
-                        </InfoWindow>
+                    <InfoWindow
+                        marker={this.state.activeMarker}
+                        visible={this.state.showingInfoWindow}>
+                        <div>
+                            <h1>{this.state.selectedPlace.name}</h1>
+                        </div>
+                    </InfoWindow>
 
-                        <Polygon
-                            paths={points}
-                            strokeColor="#0000FF"
-                            strokeOpacity={0.8}
-                            strokeWeight={2}
-                            fillColor="#0000FF"
-                            fillOpacity={0.35} />
-                    </Map>
-                </div>
+                    <Polygon
+                        paths={points}
+                        strokeColor="#0000FF"
+                        strokeOpacity={0.8}
+                        strokeWeight={2}
+                        fillColor="#0000FF"
+                        fillOpacity={0.35} />
+                </Map>
             </div>
-        );
-      
-    // } else {
-    //     return (<div>no location</div>);
-    // }
+        </div>
+    );
   }
 }
 
 export default GMap;
-
-// /* Connecting Map to State Shape*/
-// const mapStateToProps = (state, ownProps) => ({
-//     userId: state.session.id
-// });
-
-// const mapDispatchToProps = (dispatch) => ({
-//     // track: (location) => dispatch(track(location))
-// });
-
-// const MapContainer = connect(mapStateToProps, mapDispatchToProps)(GMap);
-
-/* Wrapped MapContainer with Google API*/
-// export default GoogleApiWrapper({
-//   apiKey: (gAPI)
-// })(MapContainer)
