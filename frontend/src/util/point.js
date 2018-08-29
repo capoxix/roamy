97450af3bc9dbd29b3c546cc6d94d211a7aab44f
Kevin <kevin.ou705@gmail.com>
2018-08-29 09:46:01 -0700

    Refactor initendpoints to be a instance method from origin

--- frontend/src/util/point.js
+++ frontend/src/util/point.js
@@ -1,58 +1,44 @@
 class Point {
-    constructor({lat, lng, minutes, angle}){
-      this.lat = lat;
-      this.lng = lng;
-      this.minutes = minutes;
-      this.angle = angle;
-      this.draw = false;
-    }
-    // this.distance = distance;
-    // this.time= time;
-    // rescaleLong (resTime) {
-    //   newScale = (minutes * (Math.abs(this.long - long)) / resTime);
-    //   this.long = newScale;
-    // }
-    
-    // rescalePoint (resTime) {
-    //   point.long = long+difference;
-    // }
-    static createEndPoint(startPoint, currentAngle, dLat, dLng) {
-      let pLng = startPoint.lng + (dLng * Math.cos((currentAngle * Math.PI)/ 180))
-      let pLat = startPoint.lat + (dLat * Math.sin((currentAngle * Math.PI)/ 180))
-      const newPoint = new Point({
-        lat: pLat,
-        lng: pLng,
-        angle: currentAngle
-      })
-      return newPoint
-    }
-    static initEndPoints(lat, lng, minutes) {
-      const pointsArr = [];
-    
-      const startPoint = new Point({
-        lat: lat,
-        lng: lng,
-        minutes: minutes
-      })
-      
-      //find the dLat dLng
-      const dLat = 0.003604 * startPoint.minutes;
-      const dLng =  0.0045402 * startPoint.minutes;
-      // debugger
-      let numPoints = 8;
-      let angle = 360 / numPoints;
-      let currentAngle = 0;
-    
-      while (currentAngle < 360) { 
-        let newPoint = Point.createEndPoint(startPoint, currentAngle, dLat, dLng)
-        pointsArr.push(newPoint)
-    
-        currentAngle += angle;
-      }
-    
-      return pointsArr;
-    }
+  constructor({lat, lng, minutes, angle}){
+    this.lat = lat;
+    this.lng = lng;
+    this.minutes = minutes;
+    this.angle = angle;
+    this.draw = false;
   }
+
+  static createEndPoint(origin, currentAngle, dLat, dLng) {
+    let pLng = origin.lng + (dLng * Math.cos((currentAngle * Math.PI)/ 180))
+    let pLat = origin.lat + (dLat * Math.sin((currentAngle * Math.PI)/ 180))
+    const newPoint = new Point({
+      lat: pLat,
+      lng: pLng,
+      angle: currentAngle
+    })
+    return newPoint
+  }
+
+  initEndPoints() {
+    const pointsArr = [];
+    
+    //find the dLat dLng
+    const dLat = 0.003604 * this.minutes;
+    const dLng =  0.0045402 * this.minutes;
+
+    let numPoints = 8;
+    let angle = 360 / numPoints;
+    let currentAngle = 0;
+  
+    while (currentAngle < 360) { 
+      let newPoint = Point.createEndPoint(this, currentAngle, dLat, dLng)
+      pointsArr.push(newPoint)
+  
+      currentAngle += angle;
+    }
+  
+    return pointsArr;
+  }
+}
 
 export default Point;
 
@@ -65,9 +51,11 @@ function cloudGenerator(lat ,lng, minutes) {
   const endPoints = [];
   
   // should pass in origin object instead
-  endPoints = Point.initEndPoints(lat, lng, minutes) 
+  endPoints = origin.initEndPoints() 
   
   // while (not perfect) {
   // do async matrix call one at a time FIRST!!!! OI
   endPoints.forEach((point) => point.adjustPoint(resMinutes))
-    
+}
+
+let o = new Point({lat: 37.7990, lng: -122.4014, minutes: 20})
