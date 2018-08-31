311c066fdd443f821a6ff1d83011d948f56f3ed7
Kevin <kevin.ou705@gmail.com>
2018-08-30 19:52:49 -0700

    Change discover routes and add test

--- routes/api/discover.js
+++ routes/api/discover.js
@@ -4,10 +4,11 @@ const Point = require('../../frontend/src/util/point.js');
 const fetch = require('node-fetch');
 
 
+router.get('/test', (req, res) => {
+  res.send('hi')
+})
 
-router.get(`/cars`, async (req, res) => {
-  // make api call using fetch
-  // origin will be created based on user input
+router.post(`/cars`, async (req, res) => {
 
   //decode(req.)
 
@@ -16,15 +17,12 @@ router.get(`/cars`, async (req, res) => {
     lng: -122.4014,
     minutes: 45
   })
-  Object.freeze(origin);
-
   // twin peaks
   // const origin = new Point({lat: 37.751387, lng: -122.446333, minutes: 15})
-  
+  Object.freeze(origin);
+
   let searches = 0;
-  let searchStr;
-  let endPoints;
-  let text;
+  let searchStr, endPoints, text;
  
   endPoints = origin.initEndPoints()
   Point.inPacific(endPoints) // check if in pacific ONLY FOR SF
@@ -33,24 +31,26 @@ router.get(`/cars`, async (req, res) => {
     searches+=1;
 
     searchStr = origin.makeSearchStr(endPoints);
-    // console.log(searchStr)
-    promise = await fetch(searchStr);
 
+    promise = await fetch(searchStr);
     text = JSON.parse(await promise.text());
 
     const addresses = text.destination_addresses;
     const times = text.rows[0].elements;
-    console.log('t')
-    console.log('t')
-    console.log('t')
-    console.log('t')
-    console.log('t')
-    console.log('t')
-    console.log(text)
+
     origin.adjustPoints(endPoints, times, addresses)
   
   }
+
   res.send(text);
 });
 
 module.exports = router;
+
+
+
+// returns a lat lng 
+// https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=&key=AIzaSyDBghaO6vALAG_-QG2SCBN8LEB_jFM6o1Q
+
+// returns the name
+// https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=&key=AIzaSyDBghaO6vALAG_-QG2SCBN8LEB_jFM6o1Q
