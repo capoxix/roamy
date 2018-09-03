c9e72d5b7fea17d53f935b5ae7c790892337ebc8
Garbo Cheng <garbocheng93@gmail.com>
2018-09-02 23:10:01 -0700

    restyle locations util

--- frontend/src/util/location_api_util.js
+++ frontend/src/util/location_api_util.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
 
-export const track = location => {
+export const track = (location) => {
     return axios.post(`api/locations/track`, location);
 };
 
-export const getFavorites = userId => {
-    return axios.get(`api/locations/favorites/${userId}`)
+export const getFavorites = (userId) => {
+    return axios.get(`api/locations/favorites/${userId}`);
 };
