667e2d54217212acbef5249bb32d902f1b6abaf4
Garbo Cheng <garbocheng93@gmail.com>
2018-09-17 11:17:25 -0700

    modify places functions

--- frontend/src/components/map/map_forms.jsx
+++ frontend/src/components/map/map_forms.jsx
@@ -11,8 +11,86 @@ class MapForm extends React.Component{
             trackName: ''
         }
     }
+
+    queryPlaces(query) {
+        if (this.state.map && query !== "") {
+          let request = {
+            location: this.state.map.getCenter(),
+            radius: "500",
+            query: query
+          };
+          let that = this;
+    
+          function returnPlaces(results, status) {
+            if (status == that.props.google.maps.places.PlacesServiceStatus.OK)
+              that.setState({ queryPlaces: results }) 
+          }
+    
+          this.state.service.textSearch(request, returnPlaces);
+        }
+      }
+
+    findPlaceAndMark(query) {
+        if (
+          this.state.map &&
+          query !== "" &&
+          query.length > 5
+        ) {
+          let request = {
+            query: query,
+            fields: [
+              "photos",
+              "formatted_address",
+              "name",
+              "rating",
+              "opening_hours",
+              "geometry"
+            ]
+          };
+    
+          let that = this;
+          /*find place when given location address or name or lat,lng */
+    
+          function findPlace(result, status) {
+              /* result is resulting places, which comes in as an array of objects
+              EX: [{formatted_address: "....",
+                    geometry: {location: {lat(), lng()}},
+                    name: "......."}]
+                    OR
+                    null if address/name cannot be found
+                    */
+    
+            /*TO ACESS LAT OR LNG, do place.geometry.location.lat() AND place.geomtry.location.lng() [shown in markFoundPlace]*/
+            if (status == that.props.google.maps.places.PlacesServiceStatus.OK) {
+              that.setState({center: { lat: result[0].geometry.location.lat(), lng: result[0].geometry.location.lng()}});
+            //   that.setState({query: ''});
+              that.setState({ foundPlace: result[0] });
+              that.markFoundPlace(result[0])
+            }
+          }
+          this.state.service.findPlaceFromQuery(request, findPlace);
+        }
+      }
+    
+      markFoundPlace(place){
+        let that = this;
+        this.setState({ clicked: { lat: place.geometry.location.lat(), lng: place.geometry.location.lng(), minutes: this.state.minutes } });
+        this.setState({
+            clickedMarker: (
+              <Marker
+                onClick={this.onMarkerClick}
+                name={place.name}
+                position={{ lat: place.geometry.location.lat(), lng: place.geometry.location.lng() }}
+                icon={{
+                  url: 'https://s3-us-west-1.amazonaws.com/sports-with-strangers-dev/clicked_icon.png',
+                  scaledSize: new that.props.google.maps.Size(30,30)
+                }}
+              />
+            )
+          })
+      }
     render(){
-        const {getCurrentLocation, findPlaceAndMark, update, discover, trackInput, userId} = this.props
+        const {getCurrentLocation, findPlaceAndMark, update, discover, trackInput, userId} = this.props;
         let userButtons =  [];
         if(this.props.userId) {
             userButtons =  [<form onSubmit={this.trackInput}>
