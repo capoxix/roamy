262847f0921addf042003d6503e563c0b1bde2e4
Garbo Cheng <garbocheng93@gmail.com>
2018-09-18 16:42:40 -0700

    restyle buttons

--- frontend/src/components/map/gmap.jsx
+++ frontend/src/components/map/gmap.jsx
@@ -368,7 +368,7 @@ class GMap extends React.Component {
                           placeholder="Favorite place name"
                           required
                         />
-                        <button type='submit'>TRACK LOCATION</button>
+                        <button type='submit'>Save Location</button>
                         </form>
                       ];
                       }
@@ -387,21 +387,21 @@ class GMap extends React.Component {
                       onChange={this.update('query')}
                       value={this.state.query}
                       placeholder="Search Place"/>
-                    <button type='button' onClick={()=>this.findPlaceAndMark()}>Find location</button>
+                    <button type='button' onClick={()=>this.findPlaceAndMark()}>Find Location</button>
                     <div className="discover-wrapper">
                       <select id="time" name="time" onChange={this.update("minutes")}>
-                        <option value="5" selected="true">5</option>
-                        <option value="10">10</option>
-                        <option value="15">15</option>
-                        <option value="20">20</option>
-                        <option value="25">25</option>
-                        <option value="30">30</option>
-                        <option value="35">35</option>
-                        <option value="40">40</option>
-                        <option value="45">45</option>
-                        <option value="50">50</option>
-                        <option value="55">55</option>
-                        <option value="60">60</option>
+                        <option value="5" selected="true">5 min</option>
+                        <option value="10">10 min</option>
+                        <option value="15">15 min</option>
+                        <option value="20">20 min</option>
+                        <option value="25">25 min</option>
+                        <option value="30">30 min</option>
+                        <option value="35">35 min</option>
+                        <option value="40">40 min</option>
+                        <option value="45">45 min</option>
+                        <option value="50">50 min</option>
+                        <option value="55">55 min</option>
+                        <option value="60">60 min</option>
                       </select>
                       <button className="discover-button" type='button' onClick={this.discover}>Discover</button>
                     </div>
