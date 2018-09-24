6b9bb7964547e7a900e7b229ff75190c860dd751
Garbo Cheng <garbocheng93@gmail.com>
2018-09-24 10:12:41 -0700

    modify reducers to remove markers when user logs out

--- frontend/src/reducers/locations_reducer.js
+++ frontend/src/reducers/locations_reducer.js
@@ -1,10 +1,14 @@
 import {RECEIVE_FAVORITE_POINTS, RECEIVE_TRACKED_INPUT} from '../actions/location_actions';
+import {RECEIVE_CURRENT_USER} from '../actions/session_actions';
+
 
 const locationsReducer = (state = [], action) => {
     Object.freeze(state);
     let newArr = state.slice();
 
     switch(action.type){
+        case RECEIVE_CURRENT_USER:
+            return [];
         case RECEIVE_FAVORITE_POINTS:
             return action.favorites;
         case RECEIVE_TRACKED_INPUT:
